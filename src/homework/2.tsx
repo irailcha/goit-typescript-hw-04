5f246107d89ca81984a9a220250003721bb6cff2
Your Name <your-email@example.com>
2023-10-28 18:28:33 +0300

    hw04

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,9 +1,25 @@
 import React, {useReducer} from "react";
 
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: string | null;
+  
+};
+
+type Action =
+  | { type: 'START_REQUEST' }
+  | { type: 'PENDING_REQUEST'}
+  | { type: 'FINISH_REQUEST'}
+  | { type: 'RESET_REQUEST'};
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
 };
+
+
+
 
 function requestReducer(state: State, action: Action): State {
   switch (action.type) {
